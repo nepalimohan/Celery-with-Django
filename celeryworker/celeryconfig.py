4363b3309b3507b0b8b933919bd67812538997de
MOHAN NEPALI <nepalimohan@outlook.com>
2024-06-20 14:44:01 +0545

    celeryworker standalone

--- celeryworker/celeryconfig.py
+++ celeryworker/celeryconfig.py
@@ -1,4 +1,18 @@
-import os
+# import os
 
-CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0") #0 refers to default db
-CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND", "redis://redis:6379/0") #0 refers to default db
+# CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0") #0 refers to default db
+# CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND", "redis://redis:6379/0") #0 refers to default db
+
+# celeryworker/celeryconfig.py
+# celeryworker2/celeryconfig.py
+
+from celery import Celery
+
+# Define the Celery application instance
+app = Celery('celeryworker')
+
+# Load configuration from Django settings
+app.config_from_object('settings', namespace='CELERY')
+
+# Automatically discover tasks in all Django apps
+app.autodiscover_tasks()
